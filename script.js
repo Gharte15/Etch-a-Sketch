cb9f3eec8540e7507d0a18668ef4f65d503a64dc
Gharte15 <70962125+Gharte15@users.noreply.github.com>
2022-10-24 14:37:15 +0200

    Added events to squares

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const gridContainer = document.querySelector('#grid-container');
 const DIMENSION = 16;
+
 function createGrid() {
     for (let i = 0; i < DIMENSION * DIMENSION; i++) {
-        const div = document.createElement('div');
-        div.classList.add("square");
-        gridContainer.appendChild(div);
+        const square = document.createElement('div');
+        square.classList.add("square");
+        square.addEventListener('click', changeSquareColor, false);
+        gridContainer.appendChild(square);
     }
 }
+
+function changeSquareColor(e) {
+    this.style.backgroundColor = "BLACK";
+} 
 
 createGrid();
