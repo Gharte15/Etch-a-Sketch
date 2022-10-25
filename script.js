ec6b367adbbad5de14f31c4f2fef2eb3e0099a32
Gharte15 <70962125+Gharte15@users.noreply.github.com>
2022-10-25 10:54:48 +0200

    Added buttons and slider

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const gridContainer = document.querySelector('#grid-container');
-const DIMENSION = 16;
+const btnEraser = document.querySelector('#eraseBtn');
+const btnClear = document.querySelector('#clearBtn');
+const sizeSlider = document.getElementById('size-slider')
 
-function createGrid() {
-    for (let i = 0; i < DIMENSION * DIMENSION; i++) {
+const DEFAULT_GRID_SIZE = 16;
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const square = document.createElement('div');
         square.classList.add("square");
-        square.addEventListener('click', changeSquareColor, false);
+        square.addEventListener('mouseover', changeColor);
+        square.addEventListener('mousedown', changeColor);
         gridContainer.appendChild(square);
     }
 }
 
-function changeSquareColor(e) {
-    this.style.backgroundColor = "BLACK";
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = "BLACK";
 } 
 
-createGrid();
+window.onload = () => {
+    createGrid(DEFAULT_GRID_SIZE);
+}
+
+
